cd7a138f6bfe85f0babaf30505f07963a1aebed3
Uyen Le <tle004@gold.ac.uk>
2018-09-15 15:18:54 +0100

    Added corresponding links to each tip

--- src/components/footerComponent/footer.js
+++ src/components/footerComponent/footer.js
@@ -7,12 +7,8 @@ class Footer extends Component {
       <footer>
         <nav>
           <ul>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact</Link>
-            </li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
           </ul>
         </nav>
       </footer>
--- src/components/pages/tip-list/tip/tip.js
+++ src/components/pages/tip-list/tip/tip.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Tip extends Component {
 
   render() {
     return (
       <div className="tip">
-            <div style={ { backgroundImage: "url(" + this.props.obj.image + ")" } }>
-              <h2>{(this.props.obj.id)}</h2>
-              <p>{this.props.obj.name}</p>
-            </div>
+        <Link to={this.props.obj.linkTo}>
+
+          <div style={ { backgroundImage: "url(" + this.props.obj.image + ")" } }>
+            <h2>{(this.props.obj.id)}</h2>
+            <p>{this.props.obj.name}</p>
+          </div>
+          
+        </Link>
       </div>
     );
   }
