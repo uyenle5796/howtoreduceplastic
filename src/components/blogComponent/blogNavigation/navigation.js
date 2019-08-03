944dff11923c7932e89472a6d6fb2b2a3590afd7
Uyen Le <hannah5796@gmail.com>
2019-08-03 22:09:03 +0100

    Add Home link to Navigation.

--- src/components/blogComponent/blogNavigation/navigation.js
+++ src/components/blogComponent/blogNavigation/navigation.js
@@ -1,19 +1,30 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Navigation extends Component {
-
   render() {
     return (
       <div className="blog-navigation">
         <div class="addthis_inline_share_toolbox"></div>
-        <br/>
-        { this.props.linkToPrevTip !== "" ?
-          <li><Link to={this.props.linkToPrevTip}>Previous Tip</Link></li> : ""
-        }
-        { this.props.linkToNextTip !== "" ?
-            <li><Link to={this.props.linkToNextTip}>Next Tip</Link></li> : ""
-        }
+        <br />
+        {this.props.linkToPrevTip !== "" ? (
+          <li>
+            <Link to={this.props.linkToPrevTip}>Previous Tip</Link>
+          </li>
+        ) : (
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+        )}
+        {this.props.linkToNextTip !== "" ? (
+          <li>
+            <Link to={this.props.linkToNextTip}>Next Tip</Link>
+          </li>
+        ) : (
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+        )}
       </div>
     );
   }
