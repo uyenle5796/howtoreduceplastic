e1124afa1bd95846b447e7c581a8af094dfff8f3
Uyen Le <hannah5796@gmail.com>
2022-06-15 15:13:01 +0100

    Updates

---
+++ precache-manifest.b0005f5830cde5cff3f4bf7654756137.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9c0768e810e3dc74311fa4ce14162a7",
+    "url": "/howtoreduceplastic/index.html"
+  },
+  {
+    "revision": "48a7f124e0a06d62961e",
+    "url": "/howtoreduceplastic/static/css/main.d362a221.chunk.css"
+  },
+  {
+    "revision": "d528945fb04863de1adf",
+    "url": "/howtoreduceplastic/static/js/2.9cfe76d4.chunk.js"
+  },
+  {
+    "revision": "48a7f124e0a06d62961e",
+    "url": "/howtoreduceplastic/static/js/main.48f1939f.chunk.js"
+  },
+  {
+    "revision": "94d7b5f6227c8d4f0253",
+    "url": "/howtoreduceplastic/static/js/runtime~main.5f5e7617.js"
+  }
+]);
